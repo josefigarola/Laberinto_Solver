6dc5c12723768155df7b4a0711817e7951f77fc7
josefigarola <jmfigarolaprado2@gmail.com>
2021-09-30 23:09:15 -0500

    Identity changes

--- lab_solver.cpp
+++ lab_solver.cpp
@@ -1,10 +1,12 @@
-// Prueba CLion
+// Jose Miguel Figarola Prado
+// A01632557
+// 30 sept 2021
+// Laberynth Solver using backtracking
 
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
-    std::cout << "Pruebaaaa";
-    std::cout << "Pruebaaaa 2.0";
+    cout << "HOLA MUNDO" << endl;
     return 0;
 }
